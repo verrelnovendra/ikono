0e47c853be34ad983c86771d04b88bb207bbe788
Khiar Zaki Maulana <74346237+khiarj4ki@users.noreply.github.com>
2024-12-11 11:00:35 +0700

    add Main.java

---
+++ ikono/src/edu/pradita/p14/connection/Main.java
@@ -0,0 +1,24 @@
+package edu.pradita.p14;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DatabaseConnection.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setTitle("Database Connection Settings");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
