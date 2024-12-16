15f88ed124e2e848664fb52b00d21398150c0a87
Hengkylin <hengky.1@student.pradita.ac.id>
2024-12-16 20:00:32 +0700

    Add files via upload

---
+++ ikono/database/ServiceItemData.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE SERVICE_ITEM_OBAT;
+USE SERVICE_ITEM_OBAT;
+CREATE TABLE master_data (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    category VARCHAR(255),
+    price VARCHAR(255),
+    quantity VARCHAR(255)
+);
