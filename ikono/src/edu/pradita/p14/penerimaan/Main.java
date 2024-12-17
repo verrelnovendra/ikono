dde6c589099c5aee37197d71d17932276814de8d
RipkiHensem <rhifqi.syahputra@gmail.com>
2024-12-17 16:06:39 +0700

    add  Main.java

---
+++ ikono/src/edu/pradita/p14/penerimaan/Main.java
@@ -0,0 +1,22 @@
+package edu.pradita.p14.penerimaan;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("penerimaan.fxml"));
+        primaryStage.setTitle("Form Penerimaan Barang");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
