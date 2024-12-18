cf5293e5983a320d794c02dca3ad0bdb990aa363
Crabby15 <ben07arshavin@gmail.com>
2024-12-18 23:31:18 +0700

    Update dah fix uas pakai sql bentley.sql

--- merk/dah fix uas pakai sql bentley.sql
+++ merk/dah fix uas pakai sql bentley.sql
@@ -1,11 +1,7 @@
 
 
-USE Pradita;
+use productdb;
 
-CREATE TABLE product (
-   
-    merek VARCHAR(255) NOT NULL,
-    kode VARCHAR(255) NOT NULL,
-    deskripsi VARCHAR(255) DEFAULT 'Uncategorized'
-    
-);
+
+
+select*from product;
